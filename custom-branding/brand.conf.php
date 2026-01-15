1009b2ba3c0cdcb03cea5ab8b0babe97ea4421e8
Salman-TCM <salman.connects@gmail.com>
2026-01-15 17:04:13 +0600

    remove brand logo

--- custom-branding/brand.conf.php
+++ custom-branding/brand.conf.php
@@ -2,7 +2,7 @@
 // BTRC Fixed Broadband QoS Monitoring System - Custom Branding
 
 return [
-    'BRAND_LOGO' => 'https://btrc.gov.bd/site-assets/images/logo.png',
+    'BRAND_LOGO' => null,
     'BRAND_LOGO_SIDEBAR' => null,
     'BRAND_LOGO_SIDEBAR_COMPACT' => null,
     'BRAND_FOOTER' => 'BTRC Fixed Broadband QoS Monitoring System',
